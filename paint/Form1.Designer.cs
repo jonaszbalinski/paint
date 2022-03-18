904f27fab074fb0485a1a38e4db02137f30d1c0f
Jonasz Balinski <->
2022-03-18 11:34:22 +0100

    Basic GUI and pencil tool

--- paint/Form1.Designer.cs
+++ paint/Form1.Designer.cs
@@ -28,13 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.radioButtonPencil = new System.Windows.Forms.RadioButton();
+            this.radioButtonLine = new System.Windows.Forms.RadioButton();
+            this.radioButtonRectangle = new System.Windows.Forms.RadioButton();
+            this.radioButtonEllipse = new System.Windows.Forms.RadioButton();
+            this.buttonChangeColor = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(5, 61);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(772, 487);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Outset;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(782, 553);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.radioButtonPencil, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.radioButtonLine, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.radioButtonRectangle, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.radioButtonEllipse, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonChangeColor, 4, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(5, 5);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(772, 48);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // radioButtonPencil
+            // 
+            this.radioButtonPencil.AutoSize = true;
+            this.radioButtonPencil.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radioButtonPencil.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.radioButtonPencil.Location = new System.Drawing.Point(3, 3);
+            this.radioButtonPencil.Name = "radioButtonPencil";
+            this.radioButtonPencil.Size = new System.Drawing.Size(148, 42);
+            this.radioButtonPencil.TabIndex = 0;
+            this.radioButtonPencil.TabStop = true;
+            this.radioButtonPencil.Text = "Pencil";
+            this.radioButtonPencil.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonLine
+            // 
+            this.radioButtonLine.AutoSize = true;
+            this.radioButtonLine.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radioButtonLine.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.radioButtonLine.Location = new System.Drawing.Point(157, 3);
+            this.radioButtonLine.Name = "radioButtonLine";
+            this.radioButtonLine.Size = new System.Drawing.Size(148, 42);
+            this.radioButtonLine.TabIndex = 1;
+            this.radioButtonLine.TabStop = true;
+            this.radioButtonLine.Text = "Line";
+            this.radioButtonLine.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRectangle
+            // 
+            this.radioButtonRectangle.AutoSize = true;
+            this.radioButtonRectangle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radioButtonRectangle.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.radioButtonRectangle.Location = new System.Drawing.Point(311, 3);
+            this.radioButtonRectangle.Name = "radioButtonRectangle";
+            this.radioButtonRectangle.Size = new System.Drawing.Size(148, 42);
+            this.radioButtonRectangle.TabIndex = 2;
+            this.radioButtonRectangle.TabStop = true;
+            this.radioButtonRectangle.Text = "Rectangle";
+            this.radioButtonRectangle.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonEllipse
+            // 
+            this.radioButtonEllipse.AutoSize = true;
+            this.radioButtonEllipse.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radioButtonEllipse.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.radioButtonEllipse.Location = new System.Drawing.Point(465, 3);
+            this.radioButtonEllipse.Name = "radioButtonEllipse";
+            this.radioButtonEllipse.Size = new System.Drawing.Size(148, 42);
+            this.radioButtonEllipse.TabIndex = 3;
+            this.radioButtonEllipse.TabStop = true;
+            this.radioButtonEllipse.Text = "Ellipse";
+            this.radioButtonEllipse.UseVisualStyleBackColor = true;
+            // 
+            // buttonChangeColor
+            // 
+            this.buttonChangeColor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonChangeColor.Location = new System.Drawing.Point(619, 3);
+            this.buttonChangeColor.Name = "buttonChangeColor";
+            this.buttonChangeColor.Size = new System.Drawing.Size(150, 42);
+            this.buttonChangeColor.TabIndex = 4;
+            this.buttonChangeColor.Text = "Color";
+            this.buttonChangeColor.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Paint";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.RadioButton radioButtonPencil;
+        private System.Windows.Forms.RadioButton radioButtonLine;
+        private System.Windows.Forms.RadioButton radioButtonRectangle;
+        private System.Windows.Forms.RadioButton radioButtonEllipse;
+        private System.Windows.Forms.Button buttonChangeColor;
     }
 }
 
--- paint/Form1.cs
+++ paint/Form1.cs
@@ -12,9 +12,58 @@ namespace paint
 {
     public partial class Form1 : Form
     {
+        private Point mousePressedPosition;
+        private List<PointF> pathPoints = new List<PointF>();
+        private bool shouldDrawPath = false;
+        private int pathPointsIterator = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            pictureBox.Image = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics g = Graphics.FromImage(pictureBox.Image);
+            g.Clear(Color.White);
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            //mousePressedPosition = e.Location;
+            shouldDrawPath = true;
+            pathPoints.Add(e.Location);
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            Graphics g = Graphics.FromImage(pictureBox.Image);
+            
+            g.DrawCurve(new Pen(Color.Red, 5), pathPoints.ToArray());
+            shouldDrawPath = false;
+            pathPoints = new List<PointF>();
+
+            pictureBox.Refresh();
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(shouldDrawPath)
+            {
+                if(e.Location.X > pathPoints.Last().X + 5 ||
+                   e.Location.X < pathPoints.Last().X - 5 ||
+                   e.Location.Y > pathPoints.Last().X + 5 ||
+                   e.Location.Y < pathPoints.Last().X - 5)
+                {
+                    if(pathPoints.Count > 5)
+                    {
+                        Graphics g = Graphics.FromImage(pictureBox.Image);
+                        g.DrawCurve(new Pen(Color.Red, 5), pathPoints.ToArray());
+                        
+
+                        pictureBox.Refresh();
+                    }
+                    pathPoints.Add(e.Location);
+                }
+            }
         }
     }
 }
